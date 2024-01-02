21e496552dd63d7d5401946ee8ad3391ecab6a4a
Archit Bansal <53554353+architbansalab@users.noreply.github.com>
2024-01-02 13:53:48 +0530

    pythonproject.py

--- pythonproject.py
+++ pythonproject.py
@@ -1,10 +1,9 @@
 import streamlit as st
-#import plotly.express as px
-#import pandas as pd
-#import os
-#import warnings
-#warnings.filterwarnings('ignore')
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
-#st.set_page_config(page_title="IndianCarMarket",page_icon=":bar_chart:",layout = "wide")
-#st.title(" :bar_chart: Indin Car Market")
-"HelloWorld"
+st.set_page_config(page_title="IndianCarMarket",page_icon=":bar_chart:",layout = "wide")
+st.title(" :bar_chart: Indin Car Market")
